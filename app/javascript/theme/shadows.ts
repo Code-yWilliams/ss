51246da0f289cfa492e5e2fa0acb0d21c3eac300
Cody Williams <cody.williams@untappd.com>
2023-12-02 17:08:57 -0500

    theme shadows

---
+++ app/javascript/theme/shadows.ts
@@ -0,0 +1,31 @@
+import { Shadows } from "@mui/material";
+
+const shadows: Shadows = [
+  "none",
+  `0 0 4px rgb(0, 0, 0, 0.25)`,
+  `0 0 8px rgb(0, 0, 0, 0.25)`,
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+];
+
+export default shadows;

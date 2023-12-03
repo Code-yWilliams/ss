f93e4b5edc2c7be8838df9fe83ad4f82ba514111
Cody Williams <cody.williams@untappd.com>
2023-12-02 22:15:46 -0500

    temp logout button

--- app/javascript/app/index.tsx
+++ app/javascript/app/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button, Link } from "@mui/material";
 
 const App = () => {
-  return <div>Hello my name is Cody</div>;
+  return (
+    <>
+      <div>Hello my name is Cody</div>
+      <Link href="/logout" variant="button">
+        Log Out
+      </Link>
+    </>
+  );
 };
 
 export default App;

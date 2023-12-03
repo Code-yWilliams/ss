1e2f3cec20097ff92741bf37f9a006711d8bc643
Cody Williams <cody.williams@untappd.com>
2023-12-02 20:30:32 -0500

    init devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'app#index'
 
+  # Unauthenticated users go to the login page
+  unauthenticated :user do
+    get '/' => redirect('/login')
+  end
+
+  # User login, logout, and forgot password routes
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'forgot-password'
+  }, controllers: {}
+
   scope '/app' do
     get '/*path', to: 'app#index'
   end

45850f5cb95020bf9e71e3f103b4fe0abac47105
Cody Williams <cody.williams@untappd.com>
2023-12-02 23:33:57 -0500

    scroll to top

--- app/javascript/app/index.tsx
+++ app/javascript/app/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "@components/ScrollToTop";
 
-const App = () => {
+type Props = {
+  config: any;
+  user: any;
+};
+
+const App = ({ config, user }: Props) => {
   return (
-    <>
+    <Router>
+      <ScrollToTop />
       <div>Hello. You are logged in. This is the SPA</div>
       <Link href="/logout" variant="button">
         Log Out
       </Link>
-    </>
+    </Router>
   );
 };
 

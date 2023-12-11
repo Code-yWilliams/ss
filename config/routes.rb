8f38d1a2bd5fd18825edfd078239e37c2d53cca0
Cody Williams <cody.williams@untappd.com>
2023-12-10 20:25:43 -0500

    use SPA login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'app#index'
 
+  get '/*path', to: 'app#index'
+
   # User login, logout, and forgot password routes
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     password: 'forgot-password'
   }, controllers: {}
-
-  # Unauthenticated users go to the login page
-  unauthenticated :user do
-    get '/sign_up' => redirect('/sign_up')
-    get '/*path' => redirect('/login')
-  end
-
-  get '/*path', to: 'app#index'
 end

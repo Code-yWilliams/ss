47ce42e35fc4cc2f03a67c2164f4a9d92af8c03c
Cody Williams <cody.williams@untappd.com>
2023-12-10 20:25:21 -0500

    add suspense and login route

---
+++ app/javascript/app/routes/index.tsx
@@ -0,0 +1,34 @@
+import React, { Suspense, lazy } from "react";
+import { Routes as RouterRoutes, Route } from "react-router-dom";
+import { LinearProgress } from "@mui/material";
+
+const HomeLayout = lazy(() => import("@app/components/HomeLayout"));
+const Login = lazy(() => import("@pages/Login"));
+const Private = lazy(() => import("@app/routes/Private"));
+
+const Routes = () => {
+  return (
+    <RouterRoutes>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<LinearProgress />}>
+            <Private>
+              <HomeLayout />
+            </Private>
+          </Suspense>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <Suspense fallback={<LinearProgress />}>
+            <Login />
+          </Suspense>
+        }
+      />
+    </RouterRoutes>
+  );
+};
+
+export default Routes;

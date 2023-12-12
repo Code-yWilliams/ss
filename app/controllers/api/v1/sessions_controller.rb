ae05d503d8b39b19dd1753123a52b4eba78db79e
Cody Williams <cody.williams@untappd.com>
2023-12-11 22:45:10 -0500

    make api session controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class SessionsController < ApiController
+      skip_before_action :authenticate_user!, only: [:create]
+      include Devise::Controllers::Helpers
+
+      def create
+        user = User.find_for_authentication(email: login_params[:email])
+        user.valid_password?(login_params[:password])
+
+        if user.errors.empty?
+          sign_in(user) if user.errors.empty?
+          render json: { user: user }, status: :ok
+        else
+          render json: { errors: user.errors }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def login_params
+        params.require(:user).permit(:email, :password, :remember)
+      end
+    end
+  end
+end

d85ec170c8d3200746a269ced769f11482442a8e
Cody Williams <cody.williams@untappd.com>
2023-12-11 22:39:54 -0500

    set csrf cookie in app controller

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,6 +2,10 @@ class AppController < ApplicationController
   layout 'app'
   
   skip_before_action :authenticate_user!
+  after_action :set_csrf_cookie
+  def set_csrf_cookie
+    cookies['X-CSRF-TOKEN'] = form_authenticity_token
+  end
 
   def index
     @config = {

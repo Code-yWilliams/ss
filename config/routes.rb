a8ca55042f3132aa2263ff77eb153c15c583afc0
Cody Williams <cody.williams@untappd.com>
2023-12-12 23:15:41 -0500

    adjust routes for easy signout in dev

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,13 @@ Rails.application.routes.draw do
 
   root 'app#index'
 
-  get '/*path', to: 'app#index'
-
-  # User login, logout, and forgot password routes
-  # We are overriding the GET routes above and rendering the SPA above
-  # but we are keeping these so we can keep using non-GET routes
   devise_for :users, path: '', path_names: {
-    sign_in: 'login',
+    # sign_in: 'login',
     sign_out: 'logout',
     password: 'forgot-password'
   }, controllers: {}
+
+  get '/*path', to: 'app#index'
 
   draw :api_routes
 end
